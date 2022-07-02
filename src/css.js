142cd6b0ebdbfe222cef4875cf95f14d1ce5fd8d
kodema5 <kodema5@outlook.com>
2022-07-02 15:51:19 -0700

    add null-check

--- src/css.js
+++ src/css.js
@@ -1,7 +1,7 @@
 
 export let getCssText = (name) => {
     let { cssRules } = Array.from(document.styleSheets)
-        .filter(a => a.href.indexOf(name)>=0 )[0]
+        .filter(a => a.href?.indexOf(name)>=0 )[0]
         || {}
 
     if (!cssRules) return ''
